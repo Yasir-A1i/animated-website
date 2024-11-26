9780f7afc768f3f44ecb94abfb64226a72c98af1
Yasir-A1i <velyfreelancer@gmai.com>
2024-11-26 06:10:43 +0500

    Made unresponsive header

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,65 @@ console.log("Developed by Yasir Ali :)");
 
 let menuIcon = document.querySelector('.menu-icon');
 let closeIcon = document.querySelector('.close-menu');
+let tl = gsap.timeline();
 
-let tl = gsap.timeline({paused:true});
-tl.to('.sidemenu',{
-    x:0,
-    duration:0.7
+tl.from('.logo',{
+    x:-200,
+    opacity:0,
+    duration:0.5
 });
-tl.from('.sidemenu *',{
-    x:200,
-    duration:0.7,
-    stagger:0.2,
-    opacity:0
-})
+let sideAnimate = gsap.to('.sidemenu', {
+    x: 0,
+    duration: 0.7,
+    paused: true //! Pausing the animation so it doesn't start automatically
+});
 
-menuIcon.addEventListener('click',function(){
-    tl.play();
-})
-closeIcon.addEventListener('click',function(){
-    console.log("helli");
-    tl.reverse();
-})
+//! Animation for side menu elements when they appear
+let staggeranimate = gsap.from('.sidemenu *', {
+    x: 200,
+    duration: 0.7,
+    stagger: 0.2,
+    opacity: 0,
+    paused: true //! Make this animation paused as well
+});
+
+//! Adding event listener for the menu icon to play the animation
+menuIcon.addEventListener('click', () => {
+    sideAnimate.play(); //! Play the sidemenu animation when the menu icon is clicked
+    staggeranimate.play(); //! Start the side menu item animation as well
+});
+
+//! Adding event listener for the close icon to reverse the animation
+closeIcon.addEventListener('click', () => {
+    sideAnimate.reverse(); //! Reverse the sidemenu animation when the close icon is clicked
+    staggeranimate.reverse(); //! Reverse the side menu item animation
+});
+
+//! Animation for navbar
+
+let lastScrollTop = 0; // Variable to store the last scroll position
+let navbar = document.querySelector('nav'); // Assuming your navbar is inside <nav> tag
+
+// Add an event listener to track scroll events
+window.addEventListener('scroll', () => {
+    let currentScroll = window.scrollY; // Get the current scroll position
+
+    // Check if the user is scrolling down or up
+    if (currentScroll > lastScrollTop) {
+        // Scrolling down
+        gsap.to(navbar, { 
+            top: '-110px', // Move navbar out of view
+            duration: 0.3 // Duration of the animation
+        });
+    } else {
+        // Scrolling up
+        gsap.to(navbar, {
+            top: '0', // Bring navbar back into view
+            duration: 0.3 // Duration of the animation
+        });
+    }
+
+    // Update the last scroll position to the current position
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Prevent negative scroll values
+});
+
